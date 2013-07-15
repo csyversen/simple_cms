3c1236ad31bb541264686ef290ce587b1e63b28a
Chris Syversen <christopher.syversen@northwestern.edu>
2013-07-15 16:27:17 -0500

    initial commit

---
+++ app/controllers/subjects_controller.rb
@@ -0,0 +1,74 @@
+class SubjectsController < ApplicationController
+  
+  layout "admin"
+
+  def index
+    list
+    render('list')
+    #redirect_to(:action => "list")
+  end
+
+  def list
+    @subjects = Subject.order("subjects.position ASC")
+  end
+
+  def show
+    @subject = Subject.find(params[:id])
+  end
+
+  def new
+    @subject = Subject.new
+    @subject.name = "default"
+  end
+
+  def create
+    # instantiate a new object using form parameters
+    @subject = Subject.new
+    @subject.name = params[:subject][:name]
+    @subject.position = params[:subject][:position]
+    @subject.visible = params[:subject][:visible]
+
+    # save the object
+    if @subject.save
+      # if the save succeeds, redirect to the list action
+      flash[:notice] = "Subject created"
+      redirect_to(:action => "list")
+    else
+      # if the save fails, redisplay the form so the user can fix problems
+      render("new")
+    end
+  end
+
+  def edit
+    @subject = Subject.find(params[:id])
+  end
+
+  def update
+    # instantiate a new object using form parameters
+    @subject = Subject.find(params[:id])
+    @subject.name = params[:subject][:name]
+    @subject.position = params[:subject][:position]
+    @subject.visible = params[:subject][:visible]
+
+    # save the object
+    if @subject.save
+      # if the save succeeds, redirect to the list action
+      flash[:notice] = "Subject updated"
+      redirect_to(:action => "show", :id => @subject.id)
+    else
+      # if the save fails, redisplay the form so the user can fix problems
+      render("edit")
+    end
+  end
+
+  def delete
+    @subject = Subject.find(params[:id])
+  end
+
+  def destroy
+    Subject.find(params[:id]).destroy
+    flash[:notice] = "Subject destroyed"
+    redirect_to(:action => "list")
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleCms::Application.config.secret_token = 'a3bfa19a7a3d4b696342391e7b8c98f59582b724a7104a87729ffe704e4dbb1326649a0a186b4bdb59d83bfa3063eff0b23319a1ba17dd190ff5c034e3546bd2'
